36ced63bb4705f0ff51a58778eb302f763f29493
estherjanesamuel <esther.jane.samuel@gmail.com>
2022-08-29 18:03:46 +0700

    update database to workshop

--- projects/go_sqlite/go_sqlite-1/go_sqlite1.go
+++ projects/go_sqlite/go_sqlite-1/go_sqlite1.go
@@ -2,18 +2,21 @@ package main
 
 import (
 	"bufio"
+	"bytes"
 	"database/sql"
+	"errors"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
 
-	_ "github.com/mattn/go-sqlite3"
 	"github.com/dixonwille/wmenu/v5"
+	_ "github.com/mattn/go-sqlite3"
 )
 func main()  {
 
-	db, err := sql.Open("sqlite3", "./names.db")
+	db, err := sql.Open("sqlite3", "./workshop.db")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -24,10 +27,10 @@ func main()  {
 		menu := wmenu.NewMenu("What would you like to do?")
 		menu.Action(func(o []wmenu.Opt) error {handleFunc(db, o); return nil})
 		
-		menu.Option("Add a new Person", 0, true, nil)
-		menu.Option("Get a Person", 1, false, nil)
-		menu.Option("Edit a Person", 2, false, nil)
-		menu.Option("Delete a Person", 3, false, nil)
+		menu.Option("Add a new recipe", 0, true, nil)
+		menu.Option("Get a recipe", 1, false, nil)
+		menu.Option("Edit a recipe", 2, false, nil)
+		menu.Option("Delete a recipe", 3, false, nil)
 		menu.Option("Exit", 4, false, nil)
 		err = menu.Run()
 
@@ -42,75 +45,98 @@ func handleFunc(db *sql.DB,o []wmenu.Opt) {
 	case 0:
 		reader := bufio.NewReader(os.Stdin)
 
-		fmt.Println("Enter a first name: ")
-		fn, _ := reader.ReadString('\n')
-		fn = strings.TrimSuffix(fn,"\n")
+		fmt.Println("Enter a name: ")
+		name, _ := reader.ReadString('\n')
+		name = strings.TrimSuffix(name,"\n")
 
-		fmt.Println("Enter a last name: ")
-		ln, _ := reader.ReadString('\n')
-		ln = strings.TrimSuffix(ln,"\n")
+		fmt.Println("Enter ingredients (delimeter by ','): ")
+		ingredient, _ := reader.ReadString('\n')
+		ingredient = strings.TrimSuffix(ingredient, "\n")
+		ingredients := strings.Split(ingredient, ",")
+		isHalal, _ := yesNo("isHalal ? ", reader)
+		isVegetarian, _ := yesNo("isVegetarian ? ", reader)
 
-		fmt.Println("Enter a email address: ")
-		e, _ := reader.ReadString('\n')
-		e = strings.TrimSuffix(e,"\n")
+		fmt.Println("Enter a description: ")
+		description, _ := reader.ReadString('\n')
+		description = strings.TrimSuffix(description,"\n")
 
-		fmt.Println("Enter a ip address: ")
-		ip, _ := reader.ReadString('\n')
-		ip = strings.TrimSuffix(ip,"\n")
+		fmt.Println("Enter a rating: ")
+		input, _ := reader.ReadString('\n')
+		input = strings.TrimSuffix(input,"\n")
+		input = strings.TrimSpace(input)
+		rating, _ := strconv.ParseFloat(input, 64)
 
-
-		person := person{
+		recipe := recipe{
 			id: 0,
-			first_name: fn,
-			last_name: ln,
-			email: e,
-			ip_address: ip,
+			name: name,
+			ingredients: strings.Join(ingredients, ","),
+			isHalal: isHalal,
+			isVegetarian: isVegetarian,
+			description: description,
+			rating: rating,
 		}
 
-		add(db, person)
+		add(db, recipe)
 	case 1:
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter a name to search for : ")
 		searchString, _ := reader.ReadString('\n')
     	searchString = strings.TrimSuffix(searchString, "\n")
-		people := search(db, searchString)
+		recipes := search(db, searchString)
 
-		fmt.Printf("Found %v results\n", len(people))
 
-		for _, ourPerson := range people {
-			fmt.Printf("\n----\nID: %d\nFirst Name: %s\nLast Name: %s\nEmail: %s\nIP Address: %s\n",ourPerson.id, ourPerson.first_name, ourPerson.last_name, ourPerson.email, ourPerson.ip_address)
+		for _, recipe := range recipes {
+			fmt.Printf("\n----\nID: %d\nName: %s\ningredients: %s\nisHalal: %t\nisVegetarian: %t\ndescription: %s\nrating: %v\n",recipe.id, recipe.name, recipe.ingredients, recipe.isHalal, recipe.isVegetarian, recipe.description, recipe.rating)
 		}
+
+		fmt.Println()
+		fmt.Println("---------------------------------")
+		fmt.Printf("Found %v results\n", len(recipes))
 	case 2:
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("Enter an id to edit: ")
 		id, _ := reader.ReadString('\n')
-		currentPerson := getById(db, id)
+		currentRecipe := getById(db, id)
 
-		fmt.Printf("first name (currently `%s`): ", currentPerson.first_name)
-		fn, _ := reader.ReadString('\n')
-		if fn != "\n" {
-			currentPerson.first_name = strings.TrimSuffix(fn,"\n")
+		fmt.Printf("name (currently `%s`): ", currentRecipe.name)
+		name, _ := reader.ReadString('\n')
+		if name != "\n" {
+			currentRecipe.name = strings.TrimSuffix(name,"\n")
 		}
 		
-		fmt.Printf("last name (currently `%s`): ", currentPerson.last_name)
-		ln, _ := reader.ReadString('\n')
-		if ln != "\n" {
-			currentPerson.last_name = strings.TrimSuffix(ln,"\n")
+		fmt.Printf("ingredients (currently `%s` \n): ", currentRecipe.ingredients)
+		fmt.Println("Enter ingredients (delimeter by ','): ")
+		ingredient, _ := reader.ReadString('\n')
+		ingredient = strings.TrimSuffix(ingredient, "\n")
+		ingredientList := strings.Split(ingredient, ",")
+		ingredients := strings.Join(ingredientList, ",")
+		if ingredient != "\n" {
+			currentRecipe.ingredients = ingredients
 		}
 
-		fmt.Printf("email (currently `%s`): ", currentPerson.email)
-		e, _ := reader.ReadString('\n')
-		if e != "\n" {
-			currentPerson.email = strings.TrimSuffix(e,"\n")
+		fmt.Printf("isHalal (currently `%v`): ", currentRecipe.isHalal)
+		isHalal, _ := yesNo("isHalal ? ", reader)
+		currentRecipe.isHalal = isHalal
+
+		fmt.Printf("isVegetarian (currently `%v`): ", currentRecipe.isVegetarian)
+		isVegetarian, _ := yesNo("isHalal ? ", reader)
+		currentRecipe.isVegetarian = isVegetarian
+
+		fmt.Printf("description (currently `%s`): ", currentRecipe.description)
+		description, _ := reader.ReadString('\n')
+		if description != "\n" {
+			currentRecipe.description = strings.TrimSuffix(description,"\n")
+		}
+		fmt.Printf("rating (currently `%v`): ", currentRecipe.rating)
+		input, _ := reader.ReadString('\n')
+		if input != "\n" {
+			input = strings.TrimSuffix(input,"\n")
+			input = strings.TrimSpace(input)
+			rating, _ := strconv.ParseFloat(input, 64)
+			currentRecipe.rating = rating
 		}
 
-		fmt.Printf("ip address (currently `%s`): ", currentPerson.ip_address)
-		ip, _ := reader.ReadString('\n')
-		if ip != "\n" {
-			currentPerson.ip_address = strings.TrimSuffix(ip,"\n")
-		}
-
-		affected := update(db, currentPerson)
+		affected := update(db, currentRecipe)
 
 		if affected == 1 {
 			fmt.Println("one row affected (updated)")
@@ -131,8 +157,36 @@ func handleFunc(db *sql.DB,o []wmenu.Opt) {
 	}
 }
 
+func yesNo(s string, r *bufio.Reader) (bool, error) {
+	fmt.Printf("%s [y/n] ", s)
+	input,err := r.ReadString('\n')
+	input = strings.TrimSuffix(input,"\n")
+	input = strings.TrimSpace(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+	b, err := parse(input)
+
+	if err != nil {
+		log.Fatal(err)
+		return false, err
+	}
+
+	return b, nil
+}
+
+func parse(s string) (bool, error){
+	switch strings.ToLower(s) {
+	case "y":
+		return true, nil
+	case "n":
+		return false, nil
+	}
+	return false, errors.New("invalid keypress, it does not match a boolean")
+}
+
 func deleteById(db *sql.DB, id string) int64{
-	stmt, _ := db.Prepare("DELETE FROM people WHERE id=?")
+	stmt, _ := db.Prepare("DELETE FROM recipes WHERE id=?;")
 	res, err := stmt.Exec(id) 
 	defer stmt.Close()
 
@@ -147,9 +201,9 @@ func deleteById(db *sql.DB, id string) int64{
 	return affected
 }
 
-func update(db *sql.DB, person person) int {
-	stmt, _ := db.Prepare("UPDATE people SET first_name=?, last_name=?, email=?, ip_address=? WHERE id = ?;	")
-	res, err := stmt.Exec(person.first_name, person.last_name, person.email, person.ip_address, person.id) 
+func update(db *sql.DB, recipe recipe) int {
+	stmt, _ := db.Prepare("UPDATE recipes SET name=?, ingredients=json_array(?), isHalal=?, isVegetarian=?, description=?, rating=? WHERE id = ?;")
+	res, err := stmt.Exec(recipe.name, recipe.ingredients, recipe.isHalal, recipe.isVegetarian, recipe.description, recipe.rating, recipe.id) 
 	defer stmt.Close()
 
 	if err != nil {
@@ -163,8 +217,8 @@ func update(db *sql.DB, person person) int {
 	return int(affected)
 }
 
-func getById(db *sql.DB, id string) person {
-	rows, err := db.Query("SELECT id, first_name, last_name, email, ip_address FROM people WHERE id ='" + id + "'")
+func getById(db *sql.DB, id string) recipe {
+	rows, err := db.Query("SELECT id, name, ingredients, isHalal, isVegetarian, description, rating FROM recipes WHERE id ='" + id + "'")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -176,16 +230,16 @@ func getById(db *sql.DB, id string) person {
 		log.Fatal(err)
 	}
 
-	person := person{}
+	recipe := recipe{}
 	for rows.Next() {
-		rows.Scan(&person.id,&person.first_name,&person.last_name, &person.email, &person.ip_address)
+		rows.Scan(&recipe.id, &recipe.name, &recipe.ingredients, &recipe.isHalal, &recipe.isVegetarian, &recipe.description, &recipe.rating)
 	}
-	return person
+	return recipe
 }
 
-func search(db *sql.DB, searchString string) []person {
+func search(db *sql.DB, searchString string) []recipe {
 	
-	rows, err := db.Query("SELECT id, first_name, last_name, email, ip_address FROM people WHERE first_name like '%" + searchString + "%' OR last_name like '%" + searchString + "%'")
+	rows, err := db.Query("SELECT id, name, ingredients, isHalal, isVegetarian, description, rating FROM recipes WHERE name like '%" + searchString + "%' OR description like '%" + searchString + "%';")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -197,16 +251,16 @@ func search(db *sql.DB, searchString string) []person {
 		log.Fatal(err)
 	}
 
-	people := make([]person, 0)
+	recipes := make([]recipe, 0)
 
 	for rows.Next() {
-		ourPerson := person{}
-		err = rows.Scan(&ourPerson.id, &ourPerson.first_name, &ourPerson.last_name, &ourPerson.email, &ourPerson.ip_address)
+		recipe := recipe{}
+		err = rows.Scan(&recipe.id, &recipe.name, &recipe.ingredients, &recipe.isHalal, &recipe.isVegetarian, &recipe.description, &recipe.rating)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		people = append(people, ourPerson)
+		recipes = append(recipes, recipe)
 	}
 
 	err = rows.Err()
@@ -214,18 +268,73 @@ func search(db *sql.DB, searchString string) []person {
 		log.Fatal(err)
 	}
 
-	return people
+	return recipes
 }
 
-func add(db *sql.DB, person person) {
-	stmt, _ := db.Prepare("INSERT INTO people (id,first_name, last_name, email, ip_address) VALUES(?, ?, ?, ?, ?)")
-	stmt.Exec(nil, person.first_name, person.last_name, person.email, person.ip_address) 
+func add(db *sql.DB, recipe recipe) {
+	stmt, _ := db.Prepare("INSERT INTO recipes (id, name, ingredients, isHalal, isVegetarian, description, rating) VALUES (?, ?, json_array(?), ?, ?, ?, ?)")
+	ingredients := strings.Split(recipe.ingredients, ",")
+	ingredient := strings.Join(ingredients, ",")
+	stmt.Exec(nil, recipe.name, ingredient, recipe.isHalal, recipe.isVegetarian, recipe.description, recipe.rating) 
 	defer stmt.Close()
 
-	fmt.Printf("Added %v %v \n", person.first_name, person.last_name)
+	fmt.Println(SQLQueryDebugString("INSERT INTO recipes (id, name, ingredients, isHalal, isVegetarian, description, rating) VALUES (?, ?, ?, ?, ?, ?, ?)", nil, recipe.name, ingredient, recipe.isHalal, recipe.isVegetarian, recipe.description, recipe.rating))
+	fmt.Printf("Added %v  \n", recipe.name)
+}
+type recipe struct {
+	id int
+	name, ingredients, description string
+	isHalal, isVegetarian bool
+	rating float64
 }
 
-type person struct {
-	first_name, last_name, email, ip_address string
-	id int
+// SQLQueryDebugString formats an sql query inlining its arguments
+// The purpose is debug only - do not send this to the database!
+// Sending this to the DB is unsafe and un-performant.
+func SQLQueryDebugString(query string, args ...interface{}) string {
+	var buffer bytes.Buffer
+	nArgs := len(args)
+	// Break the string by question marks, iterate over its parts and for each 
+	// question mark - append an argument and format the argument according to
+	// it's type, taking into consideration NULL values and quoting strings.
+	for i, part := range strings.Split(query, "?") {
+		buffer.WriteString(part)
+		if i < nArgs {
+			switch a := args[i].(type) {
+			case int64:
+				buffer.WriteString(fmt.Sprintf("%d", a))
+			case float64:
+				buffer.WriteString(fmt.Sprintf("%v", a))
+			case bool:
+				buffer.WriteString(fmt.Sprintf("%t", a))
+			case sql.NullBool:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%t", a.Bool))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			case sql.NullInt64:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%d", a.Int64))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			case sql.NullString:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%q", a.String))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			case sql.NullFloat64:
+				if a.Valid {
+					buffer.WriteString(fmt.Sprintf("%v", a.Float64))
+				} else {
+					buffer.WriteString("NULL")
+				}
+			default:
+				buffer.WriteString(fmt.Sprintf("%q", a))
+			}
+		}
+	}
+	return buffer.String()
 }
