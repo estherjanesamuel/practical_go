61ab1805ac47d7a6778a1df17cffe283ca9865c9
estherjanesamuel <esther.jane.samuel@gmail.com>
2023-02-17 15:26:39 +0700

    login and register

--- projects/webapi/fiber1/business/guest.go
+++ projects/webapi/fiber1/business/guest.go
@@ -40,5 +40,51 @@ func (g *Guest) login(in *Guest_LoginIn) (out Guest_LoginOut) {
 
 	out.User = user.Clean()
 	return
+}
 
+type Guest_RegisterIn struct {
+	CommonIn
+	Email, Password, Name string
+}
+
+type Guest_RegisterOut struct {
+	CommonOut
+	User *model.User `json:"user,omitempty"`
+}
+
+const Guest_RegisterPath = `/guest/register`
+
+func (g *Guest) Register(in *Guest_RegisterIn) (out Guest_RegisterOut) {
+	if len(in.Email) < 3 {
+		out.SetError(400, "invalid email")
+		return
+	}
+
+	if len(in.Password) < 3 {
+		out.SetError(400, "invalid password")
+		return
+	}
+
+	if len(in.Name) < 3 {
+		out.SetError(400, "invalid name")
+		return
+	}
+
+	user := model.NewUser(g.Db)
+	if user.FindByEmail(in.Email) {
+		out.SetError(400, "email already used")
+		return
+	}
+
+	user.Email = in.Email
+	user.Name = in.Name
+	user.SetPassword(in.Password)
+
+	if  !user.Insert() {
+		out.SetError(500, "failed insert user into database")
+		return
+	}
+
+	out.User = user.Clean()
+	return
 }
